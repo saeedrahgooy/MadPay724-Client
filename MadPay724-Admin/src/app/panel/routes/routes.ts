08e4389f1ea6f29c97d845d284cac9094e636f15
saeed.rahgooy <saeed.rahgooy@gmail.com>
2020-03-22 17:49:34 +0430

    S06-E02

--- MadPay724-Admin/src/app/panel/routes/routes.ts
+++ MadPay724-Admin/src/app/panel/routes/routes.ts
@@ -2,7 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from 'src/app/auth/auth.component';
 import { PanelComponent } from '../panel.component';
 
-export const panelRoutes : Routes = [
+export const panelRoutes: Routes = [
   {
     path : 'panel' ,
     component : PanelComponent
--- MadPay724-Admin/src/app/routes/routes.ts
+++ MadPay724-Admin/src/app/routes/routes.ts
@@ -1,20 +1,20 @@
 import { Routes, RouterModule } from '@angular/router';
 
-export const adminRoutes : Routes = [
+export const adminRoutes: Routes = [
   {
     path : '' , redirectTo: '/auth/login' , pathMatch : 'full'
-   },
-   {
+  },
+  {
     path : 'auth' , redirectTo: '/auth/login' , pathMatch : 'full'
-   },
-   {
+  },
+  {
     path : 'auth/register' , redirectTo: '/auth/register' , pathMatch : 'full'
-   },
-   {
+  },
+  {
     path : 'auth/login' , redirectTo: '/auth/login' , pathMatch : 'full'
-   },
-   {
+  },
+  {
     path : 'panel' , redirectTo: '/panel' , pathMatch : 'full'
-   }
+  }
 ];
 
